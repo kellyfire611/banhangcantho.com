aceadfe35a42bc14b1732d51d8db386b2d0426e1
kellyfire611 <kellyfire611@gmail.com>
2024-04-05 20:30:30 +0700

    bổ sung trang chủ

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xin chào PHP</title>
+</head>
+<body>
+    <?php
+        echo 'Xin chào PHP';
+    ?>
+</body>
+</html>
